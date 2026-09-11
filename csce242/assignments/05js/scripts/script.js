6d3c8e9d3b5bfeb7bf55a835918b79ac65568eee
CSE-GraceMiller <gemiller89@gmail.com>
2026-09-10 22:44:51 -0400

    added speech bubble and beverage js

---
+++ csce242/assignments/05js/scripts/script.js
@@ -0,0 +1,9 @@
+//changes the speech bubble visibility if the user clciks on teh speech card
+document.getElementById("speech-card").onclick = () => {
+  document.querySelector(".speech").classList.add("visible");
+};
+
+//repeats the beverage the user chooses and a little message when a bevergae choice is chosen
+document.getElementById("beverage").onchange = (e) => {
+  document.getElementById("beverage-message").innerHTML = (e.target.value && e.target.value + ": Nice Choice!") || "";
+};
