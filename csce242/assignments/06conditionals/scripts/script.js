513900dc9a0230ad551e22bc79b1164b65f8e6ad
CSE-GraceMiller <gemiller89@gmail.com>
2026-09-15 17:35:34 -0400

    added if grade calculation in js and hide functions for excercise 1 and 2

---
+++ csce242/assignments/06conditionals/scripts/script.js
@@ -0,0 +1,31 @@
+//exercise 1 link
+document.getElementById("exercise1-link").onclick = (e) => {
+    e.preventDefault();
+    document.getElementById("exercise1").classList.remove("hidden");
+    document.getElementById("exercise2").classList.add("hidden");
+};
+
+//exercise 2 link
+document.getElementById("exercise2-link").onclick = (e) => {
+    e.preventDefault();
+    document.getElementById("exercise1").classList.add("hidden");
+    document.getElementById("exercise2").classList.remove("hidden");
+};
+
+//when you change the number of days you intend to miss
+//show a message based on how many days are missed
+document.getElementById("txt-num-days").onkeyup = (e) => {
+    const numDays = parseInt(e.target.value);
+    const pMessage = document.getElementById("p-missing-message");
+    const percentage = (numDays / 25) * 7;
+
+    if(numDays <= 5){
+        pMessage.innerHTML = `You will lose ${percentage.toFixed(1)}% of your final grade for skipping ${numDays} days.`;
+    } else if(numDays <= 10){
+        pMessage.innerHTML = `You will lose ${percentage.toFixed(1)}% of your final grade for skipping ${numDays} days. This is starting to affect your grade.`;
+    } else if(numDays <= 15) {
+        pMessage.innerHTML = `You will lose ${percentage.toFixed(1)}% of your final grade for skipping ${numDays} days. Your attendance is becoming a concern.`;
+    } else {
+        pMessage.innerHTML = `You will lose ${percentage.toFixed(1)}% of your final grade for skipping ${numDays} days. This is a significant amount of missed class.`;
+    }
+};
