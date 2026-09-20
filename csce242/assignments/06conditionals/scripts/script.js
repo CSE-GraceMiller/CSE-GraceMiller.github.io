6d5f0d43b7e96fa00c09be1437e745dc7fe61282
CSE-GraceMiller <gemiller89@gmail.com>
2026-09-20 10:23:14 -0400

    added in date function for dayls left excercize 2, also added taggle and toggle styling

--- csce242/assignments/06conditionals/scripts/script.js
+++ csce242/assignments/06conditionals/scripts/script.js
@@ -1,13 +1,26 @@
+//menu toggle
+document.getElementById("toggle-nav").onclick = () => {
+    const menu = document.getElementById("menu-items");
+    const arrow = document.getElementById("toggle-nav");
+
+    menu.classList.toggle("show");
+
+    if (menu.classList.contains("show")) {
+        arrow.innerHTML = "▲";
+    } 
+    else {
+        arrow.innerHTML = "▼";
+    }
+};
+
 //exercise 1 link
 document.getElementById("exercise1-link").onclick = (e) => {
-    e.preventDefault();
     document.getElementById("exercise1").classList.remove("hidden");
     document.getElementById("exercise2").classList.add("hidden");
 };
 
 //exercise 2 link
 document.getElementById("exercise2-link").onclick = (e) => {
-    e.preventDefault();
     document.getElementById("exercise1").classList.add("hidden");
     document.getElementById("exercise2").classList.remove("hidden");
 };
@@ -19,13 +32,34 @@ document.getElementById("txt-num-days").onkeyup = (e) => {
     const pMessage = document.getElementById("p-missing-message");
     const percentage = (numDays / 25) * 7;
 
-    if(numDays <= 5){
+    if(numDays <= 5) {
         pMessage.innerHTML = `You will lose ${percentage.toFixed(1)}% of your final grade for skipping ${numDays} days.`;
-    } else if(numDays <= 10){
+    } 
+    else if(numDays <= 10) {
         pMessage.innerHTML = `You will lose ${percentage.toFixed(1)}% of your final grade for skipping ${numDays} days. This is starting to affect your grade.`;
-    } else if(numDays <= 15) {
+    } 
+    else if(numDays <= 15) {
         pMessage.innerHTML = `You will lose ${percentage.toFixed(1)}% of your final grade for skipping ${numDays} days. Your attendance is becoming a concern.`;
-    } else {
+    } 
+    else {
         pMessage.innerHTML = `You will lose ${percentage.toFixed(1)}% of your final grade for skipping ${numDays} days. This is a significant amount of missed class.`;
     }
 };
+
+//calculates how many days are left in the semester
+const today = new Date();
+const endOfSemester = new Date(today.getFullYear(), 11, 4);
+const daysLeft = Math.round((endOfSemester - today) / (1000 * 60 * 60 * 24));
+
+document.getElementById("p-days-left").innerHTML =
+    `You have ${daysLeft} days left in the semester`;
+
+    if (daysLeft <= 100) {
+        document.getElementById("p-count-message").innerHTML =
+        "Not time to start counting down yet.";
+    }
+
+    if (daysLeft <= 50) {
+        document.getElementById("p-count-message").innerHTML =
+        "Almost there!";
+    }
